4b93d9d9e2013045ebf95b7be0eb4f7cbf3462d1
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-10-30 10:30:59 -0300

    cors fixed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
     cors({
       credentials: true,
-      origin: ['https://myholisticclinic.herokuapp.com/']
+      origin: ['https://myholisticclinic.herokuapp.com']
     })
 );
 
